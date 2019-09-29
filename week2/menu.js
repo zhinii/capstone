f6f815365a3881082ab421af415004b2a90efa96
zhinii <hansen.c.ralph@gmail.com>
2019-09-29 17:46:23 -0400

    Add files via upload

---
+++ week2/menu.js
@@ -0,0 +1,16 @@
+var prep = document.getElementById('prep');
+var step1 = document.getElementById('step1');
+var step2 = document.getElementById('step2');
+var step3 = document.getElementById('step3');
+var step4 = document.getElementById('step4');
+var step5 = document.getElementById('step6');
+var step6 = document.getElementById('step6');
+var step7 = document.getElementById('step7');
+var step8 = document.getElementById('step8');
+
+var allSteps = document.getElementsByClassName('container');
+
+// window.onload = function(){
+// 	for (var i = 0; i < allSteps.length; i ++)
+// 	allSteps[i].style.visibility = 'hidden';
+// }
