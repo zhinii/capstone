abbe834c2298df202c99ba809080f30c826bc273
zhinii <hansen.c.ralph@gmail.com>
2019-11-03 12:06:24 -0500

    Add files via upload

---
+++ week7/scene.js
@@ -0,0 +1,270 @@
+
+const scene = new THREE.Scene();
+      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+      var canvas = document.querySelector("#objectScene")
+      var renderer = new THREE.WebGLRenderer({canvas});
+   var loader = new THREE.GLTFLoader();
+
+
+        const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+    scene.background = new THREE.Color('grey');
+      const controls = new THREE.OrbitControls( camera, canvas );
+      
+ camera.position.z = 8;
+  camera.position.y = 8;
+      controls.update();
+
+//add model object eher
+
+
+loader.load('head.glb',function(gltf){
+
+	var obj = gltf.asset;
+    scene.add(obj);  
+    });
+
+
+     
+     loader.load('s1arrow.glb',function(gltf){
+
+    scene.add(gltf.scene);  
+    	gltf.scene.visible = false;
+
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp1'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+
+    });
+     
+     loader.load('s1obj.glb',function(gltf){
+    scene.add(gltf.scene);  
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp1'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+
+    });
+
+         loader.load('s2arrow.glb',function(gltf){
+    scene.add(gltf.scene);  
+     gltf.scene.visible = false;
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp2'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+     loader.load('s2obj.glb',function(gltf){
+    scene.add(gltf.scene);  
+  stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp2'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+
+    });
+     
+         loader.load('s3arrow.glb',function(gltf){
+    scene.add(gltf.scene);  
+     gltf.scene.visible = false;
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp3'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+     loader.load('s3obj.glb',function(gltf){
+    scene.add(gltf.scene);  
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp3'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+         loader.load('s4arrow.glb',function(gltf){
+    scene.add(gltf.scene);  
+     gltf.scene.visible = false;
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp4'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+     loader.load('s4obj.glb',function(gltf){
+    scene.add(gltf.scene);  
+
+     stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp4'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+
+         loader.load('s5arrow.glb',function(gltf){
+    scene.add(gltf.scene);  
+     gltf.scene.visible = false;
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp5'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+     loader.load('s5obj.glb',function(gltf){
+    scene.add(gltf.scene);  
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp5'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+         loader.load('s6arrow.glb',function(gltf){
+    scene.add(gltf.scene);  
+     gltf.scene.visible = false;
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp6'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+     loader.load('s6obj.glb',function(gltf){
+    scene.add(gltf.scene);  
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp6'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+
+         loader.load('s7arrow.glb',function(gltf){
+    scene.add(gltf.scene);  
+     gltf.scene.visible = false;
+
+    stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp7'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+     loader.load('s7obj.glb',function(gltf){
+    scene.add(gltf.scene);  
+
+     stepList.addEventListener('click', function(){
+    for (var i = 0; i < allSteps.length; i ++)
+	if (event.target.id === 'sp7'){
+		gltf.scene.visible = true;
+	}
+	else {
+		gltf.scene.visible = false;
+	}
+	});
+    });
+     
+     
+
+
+     //end model object code here
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const pixelRatio = window.devicePixelRatio;
+    const width  = canvas.clientWidth  * pixelRatio | 0;
+    const height = canvas.clientHeight * pixelRatio | 0;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+      var animate = function () {
+        requestAnimationFrame( animate );
+      controls.update();
+
+        renderer.render( scene, camera );
+      };
+
+
+      animate();
